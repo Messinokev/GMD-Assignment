d4c5538dd6023d7c4c6135fe24cc90ab170c52d2
Anett Bianka Barát <58390834+Kilgarra@users.noreply.github.com>
2022-02-28 14:48:04 +0100

    running+idle+basic movement(together)

---
+++ GMD_Assignment/Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public Transform groundCheck;
+
+    public LayerMask groundLayer;
+
+    private float horizontal;
+
+    private float speed = 8f;
+
+    private float jummpingPower = 16f;
+
+    private bool isFacingRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+    
+    
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+}
