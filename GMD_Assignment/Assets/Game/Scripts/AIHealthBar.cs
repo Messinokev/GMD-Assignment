cc926aaaf86306c2fd6b2323638f794da4d743b6
KevinPongo <messinokev@gmail.com>
2022-04-28 22:25:09 +0200

    'merge' Anett's branch to main manually

---
+++ GMD_Assignment/Assets/Game/Scripts/AIHealthBar.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIHealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Color lowHealth;
+    public Color highHealth;
+    public Vector3 offset;
+
+    void Update()
+    {
+        if (Camera.main != null)
+            slider.transform.position = Camera.main.WorldToScreenPoint(transform.parent.position + offset);
+    }
+
+
+    public void SetHealth(float health, float maxHealth)
+    {
+        slider.gameObject.SetActive(health < maxHealth);
+        slider.value = health;
+        slider.maxValue = maxHealth;
+        slider.fillRect.GetComponentInChildren<Image>().color =
+            Color.Lerp(lowHealth, highHealth, slider.normalizedValue);
+    }
+}
