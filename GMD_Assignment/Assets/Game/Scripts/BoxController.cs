529f4577f81cabc916a9d9a6d138dfeb1da0fb34
Anett Bianka Barát <58390834+Kilgarra@users.noreply.github.com>
2022-05-08 15:23:26 +0200

    Crate working

--- GMD_Assignment/Assets/Game/Scripts/BoxController.cs
+++ GMD_Assignment/Assets/Game/Scripts/BoxController.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BoxController : MonoBehaviour
 {
     public Animator animator;
+
+    public static int coinCount;
+    private TextMeshProUGUI coinsText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinsText = GameObject.Find("CoinText").GetComponent<TextMeshProUGUI>();
+
+        LoadCoinCount();
+
+        SetCoinsText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LoadCoinCount();
     }
 
     public void SmashBox()
@@ -26,6 +34,20 @@ public class BoxController : MonoBehaviour
     IEnumerator WaitForSmashAnimation()
     {
         yield return new WaitForSeconds(animator.GetCurrentAnimatorClipInfo(0).Length);
+        coinCount+=3;
+        PlayerPrefs.SetInt("Coins", coinCount);
+        SetCoinsText();
         Destroy(gameObject);
+    }
+
+    public void LoadCoinCount()
+    {
+        coinCount = PlayerPrefs.GetInt("Coins");
+        SetCoinsText();
+    }
+
+    private void SetCoinsText()
+    {
+        coinsText.text = coinCount.ToString();
     }
 }
